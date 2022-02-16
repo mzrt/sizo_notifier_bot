dccba428e7401d3b631f414dd6eb54250c72f939
Evgeniy Tortumashev <mocart1567@gmail.com>
2022-02-16 09:18:42 +0700

    Добавил названия дней недели к датам

--- test_utils.py
+++ test_utils.py
@@ -1,11 +1,15 @@
 import unittest
-from utils import datePeriodName
+from utils import datePeriodName, weekDayStr
 
 class DatePeriodNameTestCase(unittest.TestCase):
 
     def test_days(self):
         self.assertEqual('5 дней', ''.join(datePeriodName({'d':5})))
+    def test_weekDayName(self):
+        self.assertEqual('понедельник', weekDayStr('07.02.2022'))
+        self.assertEqual('понедельник', weekDayStr('7.2.2022'))
+        self.assertEqual('воскресенье', weekDayStr('13.2.2022'))
 
 # Executing the tests in the above test case class
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
