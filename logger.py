b3016d01ece35767bd3e49a723a4da2c61276e89
Evgeniy Tortumashev <mocart1567@gmail.com>
2022-06-23 15:39:23 +0700

    Fix parser

--- logger.py
+++ logger.py
@@ -2,17 +2,9 @@ import logging
 from logging.handlers import RotatingFileHandler
 import os
 from dotenv import dotenv_values
+from config import config, devMode
 
-devMode = 'app' in os.environ and os.environ['app']=="dev"
-
-config = {
-    **dotenv_values(".env.shared"),
-    **dotenv_values(".env.secret"),
-    **dotenv_values(".env.shared.local"),
-    **dotenv_values(".env.secret.local"),
-    **(dotenv_values(".env.development.local") if devMode else {}),
-    **os.environ,  # override loaded values with environment variables
-}
+level = logging.DEBUG if devMode else logging.INFO
 logFile = config['LOG_FILENAME_BOT']
 logFileRequest=config['LOG_FILENAME_PARSER']
 
@@ -21,12 +13,12 @@ log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(line
 my_handler_bot = RotatingFileHandler(logFile, mode='a', maxBytes=5*1024*1024,
                                  backupCount=2, encoding='utf-8', delay=0)
 my_handler_bot.setFormatter(log_formatter)
-my_handler_bot.setLevel(logging.DEBUG)
+my_handler_bot.setLevel(level)
 
 my_handler_parser = RotatingFileHandler(logFileRequest, mode='a', maxBytes=5*1024*1024,
                                  backupCount=2, encoding='utf-8', delay=0)
 my_handler_parser.setFormatter(log_formatter)
-my_handler_parser.setLevel(logging.INFO)
+my_handler_parser.setLevel(level)
 
 app_log_bot = logging.getLogger('bot')
 app_log_bot.setLevel(logging.DEBUG)
--- requestParser.py
+++ requestParser.py
@@ -2,31 +2,20 @@ from errno import ECONNABORTED, ECONNREFUSED, ECONNRESET, EPIPE, ESHUTDOWN
 import os
 import re, requests, json
 from lxml import html
-import logging
 from dotenv import dotenv_values
 
-devMode = 'app' in os.environ and os.environ['app']=="dev"
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+# Local imports
+from logger import app_log_bot as logging
+from config import config
 
-config = {
-    **dotenv_values(".env.shared"),
-    **dotenv_values(".env.secret"),
-    **dotenv_values(".env.shared.local"),
-    **dotenv_values(".env.secret.local"),
-    **(dotenv_values(".env.development.local") if devMode else {}),
-    **os.environ,  # override loaded values with environment variables
-}
-
-dataFileName = config['DATA_JSON_FILENAME']
 url = config['URL']
-level = logging.DEBUG if devMode else logging.INFO
-logging.basicConfig(filename=config['LOG_FILENAME_PARSER'], encoding='utf-8', level=level)
+dataFileName = config['DATA_JSON_FILENAME']
 
 logging.info(f'config {json.dumps(config, indent=4)}')
 def load_data():
     try:
         # establishing session
-        session = requests.Session() 
+        session = requests.Session()
         session.headers.update({
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'
         })
@@ -74,7 +63,7 @@ def getDaysList(response):
 from schedule import every, repeat, run_pending
 import time
 
-@repeat(every(int(config['REQUEST_MINUTES_INTERVAL'])).minutes)
+@repeat(every(int(config['REQUEST_SECONDS_INTERVAL'])).seconds)
 def job():
     r = load_data()
     if(len(r)>0):
